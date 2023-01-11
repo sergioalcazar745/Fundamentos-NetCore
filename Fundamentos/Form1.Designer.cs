e1670731fdd3f48d33da52a73aadbbefa22241f4
sergioalcazar745 <sergioalcazar745@gmail.com>
2023-01-11 09:56:35 +0100

    Add project files.

---
+++ Fundamentos/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Fundamentos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnPulsar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNombre.Location = new System.Drawing.Point(45, 49);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(93, 23);
+            this.lblNombre.TabIndex = 0;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(144, 49);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(281, 23);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnPulsar
+            // 
+            this.btnPulsar.BackColor = System.Drawing.Color.Red;
+            this.btnPulsar.Location = new System.Drawing.Point(169, 94);
+            this.btnPulsar.Name = "btnPulsar";
+            this.btnPulsar.Size = new System.Drawing.Size(177, 42);
+            this.btnPulsar.TabIndex = 2;
+            this.btnPulsar.Text = "Pulsar";
+            this.btnPulsar.UseVisualStyleBackColor = false;
+            this.btnPulsar.Click += new System.EventHandler(this.btnPulsar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(504, 181);
+            this.Controls.Add(this.btnPulsar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.lblNombre);
+            this.Name = "Form1";
+            this.Text = "Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label lblNombre;
+        private TextBox txtNombre;
+        private Button btnPulsar;
+    }
+}
---
+++ Fundamentos/Form1.cs
@@ -0,0 +1,24 @@
+namespace Fundamentos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPulsar_Click(object sender, EventArgs e)
+        {
+            //PROPIEDADES DE TIPO PRIMITICO
+            this.txtNombre.Width = 150;
+            this.btnPulsar.Text = "Botón pulsado";
+
+            //PROPIEDADES DE TIPO OBJETO
+            this.txtNombre.Location = new Point(70, 100);
+
+            //PROPIEDADES DE TIPO ENUMERADAS
+            this.txtNombre.TextAlign = HorizontalAlignment.Center;
+            this.BackColor = Color.Beige;
+        }
+    }
+}
